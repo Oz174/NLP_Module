53480ea3140f0a689f3e04f9e12eb0308fb02cf1
Oz174 <ahmedtarek1754@gmail.com>
2024-03-07 12:34:16 +0200

    soundrecorder optimizations

--- ready_modules/s_record.py
+++ ready_modules/s_record.py
@@ -1,26 +1,38 @@
-import speech_recognition as sr
 import os
+import speech_recognition as sr
+from concurrent.futures import ThreadPoolExecutor
 
 
 class SoundRecorder:
     @staticmethod
+    def process_audio(audio_file):
+        r = sr.Recognizer()
+        try:
+            with sr.AudioFile(audio_file) as source:
+                audio_ans = r.record(source)
+            sentence = r.recognize_google(audio_ans)
+            return sentence
+        except Exception as e:
+            print(f'Error processing {audio_file}: {e}')
+            return None
+
+    @staticmethod
     def get_answers(path_to_answers):
         answers = {}
-        r = sr.Recognizer()
-        answers_files = [os.path.join(path_to_answers, f) for f in os.listdir(
+        audio_files = [os.path.join(path_to_answers, f) for f in os.listdir(
             path_to_answers) if f.endswith('.wav')]
-        for audio in answers_files:
-            sentence = ""
-            # Use  the  reсоgnize_google()  funсtiоn  tо  reсоgnize  the  аudiо
-            with sr.AudioFile(audio) as source:
-                audio_ans = r.record(source)
-            try:
-                sentence = r.recognize_google(audio_ans)
-                # In order to take the question number only from the file name
-                answers[audio[-6:-4]] = sentence
-            except Exception as e:
-                print('Error:  ' + str(e))
-        del sentence
-        del answers_files
-        del audio_ans
+        with ThreadPoolExecutor() as executor:
+            futures = {executor.submit(
+                SoundRecorder.process_audio, audio_file): audio_file
+                for audio_file in audio_files}
+            for future in futures:
+                audio_file = futures[future]
+                sentence = future.result()
+                if sentence is not None:
+                    answers[os.path.basename(audio_file)[-6:-4]] = sentence
         return answers
+
+
+# Example usage:
+answers = SoundRecorder.get_answers("audio samples/interviewee_answer")
+print(answers)
